f8d35c894e8e016cc819ce806e24c09e02ea1ab4
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-01-17 00:05:46 +0900

    more info ls

--- listSegments/ls.go
+++ listSegments/ls.go
@@ -6,7 +6,23 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 )
+
+var layout = "Mon 01/02/06 15:04:05"
+
+func timeFmt(fns os.FileInfo) string {
+	return fns.ModTime().Format(layout)
+}
+
+func sizeFMT(fns os.FileInfo) string {
+	int64 := strconv.FormatInt(int64(fns.Size()), 10)
+	spaces := " ";
+	for i := 0; i < (19 - len(int64)); i++ {
+		spaces += " ";
+	}
+	return int64 + spaces
+}
 
 func Path(path string) {
 	dir, err := os.Getwd()
@@ -44,9 +60,10 @@ type fi struct{ FileInfos }
 func sortFile(fns []os.FileInfo) {
 	for _, fns := range fns {
 		if fns.IsDir() {
-			colorPrint.Out(fns.Name(), "cyan")
+			colorPrint.Out(fns.ModTime().Format("Mon 01/02/06 15:04:05")  + " "  + fns.Name(), "cyan")
 		} else {
-			defer colorPrint.Out(fns.Name(), "white")
+			defer colorPrint.Out(timeFmt(fns) + " " + sizeFMT(fns) + " " + fns.Name(), "white")
 		}
 	}
 }
+
